39fd684b3eaa0ab9bdf26f31e71c0fa883587a40
docodocod <dongan0618@naver.com>
2023-08-07 01:12:04 +0900

    db_connection_modify

---
+++ src/loader/app.js
@@ -0,0 +1,66 @@
+import express from "express";
+import nunjucks from "nunjucks";
+
+// 우선 config파일을 불러온다
+import config from "../../config/config.json" assert {type: "json"} ;
+
+
+// 기본값은 개발모드로 설정한다
+const Config = config.development;
+
+import dbLoader from "../modules/maria.js"
+import indexRouter from "../routes/index.js";
+
+const app = express();
+
+dbLoader();
+// const maria = require('./modules/maria');
+// maria.connect(); //마리아 db 연결
+
+app.set('port', Config.PORT || 8001); //Config.PORT를 앞에 붙여준 이유는 배포와 개발할때 서로 다른 포트를 사용할거라서
+app.set('view engine', 'html');
+nunjucks.configure('views', { //nunjucks 설정방법
+    express: app,
+    watch: true,
+});
+
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended: true}));
+
+// app.use(cookieParser(Config.COOKIE_SECRET));
+
+/*app.use(session({
+    resave: false,
+    saveUninitialized
+: false,
+    secret: Config.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+}));*/
+
+app.use("/", indexRouter);
+
+app.use((req, res, next) => {
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+const server = app.listen(Config.port, () => {
+    const {address, port} = server.address();
+    console.log('Server Listening on 127.0.0.1:' + Config.PORT);
+});
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,27 +1,38 @@
-const express=require('express');
-const router=express.Router();
-router.use((req,res,next)=>{
-    res.locals.user=null;
-    res.locals.followerCount=0;
-    res.locals.followingCount=0;
-    res.locals.followerIdList=[];
+import {Router} from "express";
+import mysql from "../../mysql.js";
+
+
+const router = Router();
+
+
+const data1 = '동안'
+const result = await mysql.asyncFunction(`select * from user where 이름 = ${data1}`);
+
+console.log(result);
+
+router.use((req, res, next) => {
+    res.locals.user = null;
+    res.locals.followerCount = 0;
+    res.locals.followingCount = 0;
+    res.locals.followerIdList = [];
     next();
 });
 
-router.get('/profile',(req,res)=>{
-    res.render('profile',{title: '내 정보 - NodeTwitter'});
+router.get('/profile', (req, res) => {
+    res.render('profile', {title: '내 정보 - NodeTwitter'});
 });
 
-router.get('/join',(req,res)=>{
-    res.render('join',{title:'회원가입 -NodeTwitter'});
+router.get('/join', (req, res) => {
+    res.render('join', {title: '회원가입 -NodeTwitter'});
 });
 
-router.get('/',(req,res,next)=>{
-    const twits=[];
-    res.render('main',{
+router.get('/', (req, res, next) => {
+    const twits = [];
+    res.render('main', {
         title: 'NodeTwitter',
         twits,
     });
 });
 
-module.exports=router;
+
+export default router;
