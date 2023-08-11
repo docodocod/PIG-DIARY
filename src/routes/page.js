ee65e7074a7b6a22ddc4c80fb6d656f3344ec5c9
docodocod <dongan0618@naver.com>
2023-08-11 16:44:12 +0900

    router_modify

--- src/routes/page.js
+++ src/routes/page.js
@@ -3,15 +3,14 @@ const router = Router();
 import {getSelection, postInsert, postUpdate} from "../modules/maria.js";
 
 router.get("/profile/:id", function(req,res){
-    const id=req.params.id;
-    console.log(id);
-    const profile = `select email,nick from user where email=${id}`;
+    const p=req.params.id;
+/*    const profile = `select email,nick from user where email=${p.email}`;
     console.log(profile);
-    const resultSet = getSelection(profile);
+    const resultSet = getSelection(profile);*/
     res.send("성공");
 });
 router.post("/join",function(req,res){
-    const {email,nick,password}=req.params;
+    const {email,nick,password}=req.body;
     const newUser=`insert into user (email,nick,password) values(${email},${nick},${password})`;
     console.log(newUser);
     const resultSet=postInsert(newUser);
