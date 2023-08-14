370fb15906de0c5d1c6b9a24816cbf2713033459
docodocod <dongan0618@naver.com>
2023-08-14 19:34:49 +0900

    login_method_create

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,47 +1,30 @@
-const bcrypt = require('bcrypt');
-const passport = require('passport');
+import {getUserLogin, postUserJoin} from "../dao/user.js";
+import {getSelection, postInsert} from "../modules/maria.js";
+import bcrypt from "bcrypt";
 
-exports.join = async (req, res, next) => {
-    const { email, nick, password } = req.body;
+export async function join(req, res, next) {
     try {
-        const exUser = await User.findOne({ where: { email } });
-        if (exUser) {
-            return res.redirect('/join?error=exist');
-        }
-        const hash = await bcrypt.hash(password, 12);
-        await User.create({
-            email,
-            nick,
-            password: hash,
-        });
-        return res.redirect('/');
+        const {email, nick, password} = req.body;
+        const query = postUserJoin(email, nick, password);
+        const result = postInsert(query);
+        console.log(result);
+        return result;
+        res.send("성공");
+    }catch(error){
+        console.error(error);
+    }
+};
+export async function login(req,res,next) {
+    try {
+        const {email, password} = req.body;
+        console.log(email,password);
+        const query = getUserLogin(email, password);
+        const result = getSelection(query);
+        console.log(result);
+        return result;
+        res.send("로그인 성공");
     } catch (error) {
         console.error(error);
-        return next(error);
     }
-}
-
-exports.login = (req, res, next) => {
-    passport.authenticate('local', (authError, user, info) => {
-        if (authError) {
-            console.error(authError);
-            return next(authError);
-        }
-        if (!user) {
-            return res.redirect(`/?error=${info.message}`);
-        }
-        return req.login(user, (loginError) => {
-            if (loginError) {
-                console.error(loginError);
-                return next(loginError);
-            }
-            return res.redirect('/?머임');
-        });
-    })(req, res, next); // 미들웨어 내의 미들웨어에는 (req, res, next)를 붙입니다.
 };
 
-exports.logout = (req, res) => {
-    req.logout(() => {
-        res.redirect('/');
-    });
-};
