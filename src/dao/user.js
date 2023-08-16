a4767f6659b3b714e7d1ace7ea7bf7dbe1c550ab
docodocod <dongan0618@naver.com>
2023-08-16 22:32:24 +0900

    comment_remove

--- src/dao/user.js
+++ src/dao/user.js
@@ -1,24 +1,4 @@
-// const mariadb=require('../modules/maria.js');
-// import bcrypt from "bcrypt";
 import sql from "mysql";
-
-
-/*
-module.exports=(req,res)=>{
-    const {email,password}=req.body;
-
-    const result=mariadb.getSelection(query);
-    return result;
-}
-*/
-/*
-*
-* controller > service > dao > service > controller
-* 질문
-* 없습니다. 이거 활용해서 api 호출까지 지금 해봐
-* 넵..
-* */
-
 export function getUserData(email){
     let query = `select email,password from user where email=${sql.escape(email)}`;
     console.log(query);
@@ -33,12 +13,12 @@ export function postUserJoin(email,nick,password){
 
 export function getUserLogin(email,password){
     let query=`select email,nick from user where email=${sql.escape(email)} and password=${sql.escape(password)}`;
-    console.log(query);
+
     return query;
 }
 
 export function getUserProfile(email){
     let query=`select email,nick from user where email=${sql.escape(email)}`;
-    console.log(query);
+
     return query;
 };
