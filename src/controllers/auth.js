357a3ef74308768187b4ce358b402200becb65f8
docodocod <dongan0618@naver.com>
2023-08-17 23:48:37 +0900

    bcrypt_add

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,23 +1,28 @@
-import {getUserLogin, postUserJoin,getUserProfile} from "../dao/user.js";
+import {getUserLogin, postUserJoin, getUserProfile} from "../dao/user.js";
 import {getSelection, postInsert} from "../modules/maria.js";
-import bcrypt from "bcrypt";
+import bcrypt, {hash} from "bcrypt";
+import {json} from "express";
 
 export async function join(req, res, next) {
     try {
         const {email, nick, password} = req.body;
-        const query = postUserJoin(email, nick, password);
+        const salt=12;
+        bcrypt.hash(password, salt, (err, encryptedPW) => {
+
+        })
+        const query = postUserJoin(email, nick, encodedPassword);
         const result = postInsert(query);
-        console.log(result);
+        res.status(200).json(result);
         return result;
-        res.send("성공");
-    }catch(error){
+    } catch (error) {
         console.error(error);
     }
 };
-export async function login(req,res,next) {
+
+export async function login(req, res, next) {
     try {
         const {email, password} = req.body;
-        console.log(email,password);
+        console.log(email, password);
         const query = getUserLogin(email, password);
         const result = getSelection(query);
         console.log(result);
@@ -28,16 +33,16 @@ export async function login(req,res,next) {
     }
 };
 
-export async function profile(req,res,next){
-    try{
-        const email=req.params.email;
+export async function profile(req, res, next) {
+    try {
+        const email = req.query.email;
         console.log(email);
-        const query=getUserProfile(email);
-        const result=getSelection(query);
-        console.log(result);
-        return result;
-        res.send("profile 확인");
-    }catch(error){
+        const query = getUserProfile(email);
+        const result = await getSelection(query);
+
+        res.status(200).json(result[0]);
+
+    } catch (error) {
         console.error(error);
     }
 }
