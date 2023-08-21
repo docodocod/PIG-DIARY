558e97ddd0fbd3a38f83ffaa2525265a564fc4f3
docodocod <dongan0618@naver.com>
2023-08-21 22:19:15 +0900

    controller_auth_modify

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,51 +1,46 @@
 import {getUserLogin, postUserJoin, getUserProfile, getUserData} from "../dao/user.js";
-import {getSelection, postInsert} from "../modules/maria.js";
+import {Select,Insert} from "../modules/maria.js";
 import bcrypt, {hash} from "bcrypt";
-import {json} from "express";
+import passport from "passport";
 
 export async function join(req, res, next) {
-    const {email, nick, password} = req.body;
-    try {
-        const user=getUserData(email);
-        const exUser= await getSelection(user);
-        console.log(exUser);
-        if(!exUser){
-            console.log("이미존재");
-           return res.redirect("/join?error=exist");
+    const {email,nick,password}=req.body;
+    try{
+        const exUser=await getUserData(email);
+        if(exUser){
+            return res.redirect('/join?error=exist');
         }
         const hash=await bcrypt.hash(password,12);
-        const query = postUserJoin(email, nick, hash);
-        await postInsert(query);
-        return res.redirect("/");
-    } catch (error) {
+        await postUserJoin(email,nick,hash);
+        return res.redirect('/');
+    }catch(error){
         console.error(error);
         return next(error);
     }
 };
 
 export async function login(req, res, next) {
-    try {
-        const {email, password} = req.body;
-        const hash=await bcrypt.hash(password,12);
-        const query = getUserLogin(email, hash);
-        const result = await getSelection(query);
-        res.status(200).json(result);
-    } catch (error) {
-        console.error(error);
-    }
+    passport.authenticate("local",(authError,user,info)=>{
+        if(authError){
+            console.log(authError);
+            return next(authError);
+        }
+        if(!user){
+            return res.redirect(`/?loginError=${info.message}`);
+        }
+        return res.login(user,(loginError)=>{
+            if(loginError){
+                console.error(loginError);
+                return next(loginError);
+            }
+            return res.redirect('/');
+        });
+    }),(req,res,next);
 };
 
-export async function profile(req, res, next) {
-    try {
-        const email = req.query.email;
-        console.log(email);
-        const query = getUserProfile(email);
-        const result = await getSelection(query);
-
-        res.status(200).json(result[0]);
-
-    } catch (error) {
-        console.error(error);
-    }
+export async function logout(req,res){
+    req.logout();
+    req.session.destroy;
+    res.redirect('/');
 }
 
