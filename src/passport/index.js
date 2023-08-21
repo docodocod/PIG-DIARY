8fbc2a30f5bc6425b729e715c6f6ed21856b9100
docodocod <dongan0618@naver.com>
2023-08-21 22:08:57 +0900

    index_done

--- src/passport/index.js
+++ src/passport/index.js
@@ -1,15 +1,18 @@
 import passport from "passport";
 import local from "./localStrategy.js";
 import setUpKakaoStrategy from "./kakaoStrategy.js";
+import {getUserData} from "../dao/user.js";
 
 
 module.exports = () => {
     passport.serializeUser((user, done) => {
-        done(null, user.id);
+        done(null, user.email);
     });
-    passport.deserializeUser((id, done) => {
+    passport.deserializeUser((email, done) => {
+        getUserData(email)
+        .then(user=>done(null,user))
+        .catch(err=>done(err));
     });
-
     local();
     setUpKakaoStrategy();
 };
