d6cfd836a92363074a94721c5d448b82a77c92cf
docodocod <dongan0618@naver.com>
2023-08-21 22:18:42 +0900

    auth_reorganization

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,26 +1,19 @@
-const express=require('express');
-const passport=require('passport');
-const bcrypt=require('bcrypt');
-const User=require('../models/user');
+import express from "express";
+import passport from "passport";
+import bcrypt from "bcrypt";
+import {postUserJoin, getUserData} from "./dao/user.js";
+import {isLoggedIn,isNotLoggedIn} from "../middlewares/index.js";
+import {join,login,logout} from "../controllers/auth.js";
 const router=express.Router();
 
-router.post('/join',async(req,res,next)=>{
-    const {email,nick,password}=req.body; //요청 받은 값들을 저장한다.
-    try{
-        const exUser=await User.findOne({where: {email}}); //해당 이메일을 db에서 찾는다.
-        if(exUser){
-            return res.redirect('/join?error=exist');
-        }
-        const hash=await bcrypt.hash(password,12);
-        await User.create({
-            email,
-            nick,
-            password: hash,
-        });
-        return res.redirect('/');
-    }catch(error){
-        console.error(error);
-        return next(error);
-    }
+router.post('/join',isNotLoggedIn,join);
+router.post("/login",isNotLoggedIn,login);
+router.get('/logout',isLoggedIn,logout);
+router.get("/kakao",passport.authenticate("kakao"));
+router.get("/kakao/callback",passport.authenticate("kakao",{
+    failureRedirect:"/",
+}),(req,res)=>{
+    res.redirect('/');
 });
+
 module.exports=router;
