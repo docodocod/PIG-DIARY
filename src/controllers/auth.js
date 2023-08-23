342f55147240ed6e177c45cec00285b5d3a764fa
docodocod <dongan0618@naver.com>
2023-08-23 20:45:10 +0900

    local_pbkdf2_done

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,7 @@
 import {postUserJoin, getUserData} from "../dao/auth.js";
 import crypto from "crypto";
+import dotenv from "dotenv";
+const Config = dotenv.config({ path: "./config/.env.app" }).parsed;
 export async function join(req, res, next) {
     const {email,nick,password}=req.body;
     try{
@@ -8,14 +10,14 @@ export async function join(req, res, next) {
         if(exUser) {
             return res.redirect('/join?error=exist');
         }
-        const salt = crypto.randomBytes(16).toString('hex'); // 무작위로 생성된 솔트 값
-        const iterations = 10; // 반복 횟수
+        const salt = Config.SALT;
+        const iterations = parseInt(Config.ITERATION);
         const keyLength = 64; // 출력 길이
         crypto.pbkdf2(password, salt, iterations, keyLength, 'sha512', (err, derivedKey) => {
             if (err) throw err;
             const hashedPassword = derivedKey.toString('hex');
             console.log('Hashed Password:', hashedPassword);
-            postUserJoin(email, nick, hashedPassword);
+            postUserJoin(email, nick, hashedPassword,salt);
         });
     }catch(error){
         console.error(error);
@@ -27,25 +29,23 @@ export async function login(req, res, next) {
     const {email, password} = req.body;
     try {
         const exUser = await getUserData(email);
-        console.log(exUser);
         if (exUser) {
-            const storedPw = exUser.password;
-            console.log("storedPw: "+storedPw);
-            const salt = crypto.randomBytes(16).toString('hex'); // 무작위로 생성된 솔트 값
-            const iterations = 10; // 반복 횟수
+            const storedPW=exUser.password;
+            const salt = Config.SALT;
+            const iterations = parseInt(Config.ITERATION);
             const keyLength = 64; // 출력 길이
             crypto.pbkdf2(password, salt, iterations, keyLength, 'sha512', (err, derivedKey) => {
                 if (err) throw err;
                 const hashedPw = derivedKey.toString('hex');
                 console.log('Hashed Password:', hashedPw);
-                    if (storedPw === hashedPw) {
+                    if ( storedPW=== hashedPw) {
                         res.status(200).send("로그인 성공");
                     } else {
                         res.status(200).send("비밀번호가 틀렸습니다.");
                     }
             });
         }else{
-            res.error("아이디가 존재하지 않습니다.");
+            res.status(200).send("아이디가 존재하지 않습니다.");
         }
     } catch (error) {
         console.error(error);
---
+++ src/dao/auth.js
@@ -0,0 +1,36 @@
+import sql from "mysql";
+import {Select,Insert,Update,Delete} from "../modules/maria.js";
+export async function getUserData(email){
+    let query = `select email,password from user where email=${sql.escape(email)}`;
+    const result=await Select(query);
+    console.log(result);
+    return result;
+};
+
+export async function getSnsData(profile_id,provider){
+    let query=`select snsId,provider from user where snsId=${sql.escape(profile_id)} and provider=${sql.escape(provider)}`;
+    const result=await Select(query);
+    console.log(result);
+    return result;
+}
+
+export async function postUserJoin(email,nick,password){
+    let query=`insert into user (email,nick,password) values(${sql.escape(email)},${sql.escape(nick)},${sql.escape(password)})`;
+    const result=await Insert(query);
+    console.log(result);
+    return result;
+};
+
+export async function postSnsJoin(email,nick,snsId,provider){
+    let query=`insert into user (email,nick,snsId,provider) values(${sql.escape(email)},${sql.escape(nick)},${sql.escape(snsId)},${sql.escape(provider)})`;
+    const result=await Insert(query);
+    console.log(result);
+    return result;
+};
+
+export async function getUserLogin(email,password){
+    let query=`select email,nick from user where email=${sql.escape(email)} and password=${sql.escape(password)}`;
+    const result=await Select(query);
+    console.log(result);
+    return result;
+};
