97f78e8c6be8ec217a81ba29faaeddba3db64a90
docodocod <dongan0618@naver.com>
2023-08-26 09:15:39 +0900

    post_modifying

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -5,23 +5,13 @@ exports.afterUploadImage = (req, res) => {
     res.json({ url: `/img/${req.file.filename}` });
 };
 
-exports.uploadPost = async (req, res, next) => {
+export async function uploadPost(req, res, next){
     const content=req.body.content;
     const image=req.body.img;
     const UserId=req.user.id;
     try {
         const post = await createPost(content,image,UserId);
         const hashtags = req.body.content.match(/#[^\s#]*/g);
-        if (hashtags) {
-            const result = await Promise.all(
-                hashtags.map(tag => {
-                    return Hashtag.findOrCreate({
-                        where: { title: tag.slice(1).toLowerCase() },
-                    })
-                }),
-            );
-            await post.addHashtags(result.map(r => r[0]));
-        }
         res.redirect('/');
     } catch (error) {
         console.error(error);
