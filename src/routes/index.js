1ecdcca5ebab24a9977257b386155ac29547be8c
docodocod <dongan0618@naver.com>
2023-09-19 23:20:57 +0900

    routes_edit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,9 @@
 import express from "express";
 const router=express.Router();
-import {isLoggedIn,isNotLoggedIn} from '../middlewares/index.js';
-import {renderMain,renderJoin} from "../controllers/index.js";
-import {renderMainRoom, renderRoom} from "../controllers/room.js";
+import {renderMain} from "../controllers/index.js";
 
-router.get('/join',isNotLoggedIn,renderJoin); //회원가입 페이지 이동
 router.get('/',renderMain); //메인 페이지 이동
-router.get('/roomMain', renderMainRoom); //채팅방 페이지 이동
+
+
+
 export default router;
--- src/routes/room.js
+++ src/routes/room.js
@@ -1,11 +1,44 @@
-import express from 'express';
+import express from "express";
+import multer from "multer";
+import path from "path";
+import fs from "fs";
+import {removeRoom} from "../service/roomDelete.js";
+import {
+    renderRoom, renderMainRoom, createRoom, enterRoom, sendChat, sendGif
+} from "../controllers/room.js";
+
 const router = express.Router();
-import {createRoom, renderMainRoom, renderRoom ,enterRoom,sendChat} from "../controllers/room.js";
 
+router.get('/', renderMainRoom); //채팅방 목록
 
-router.get('/',renderRoom);
-router.post('/',createRoom);
-router.get('/:id', enterRoom);
-router.post('/:id/chat',sendChat);
+router.get('/room', renderRoom); //채팅방 생성창
+
+router.post('/addRoom', createRoom); //채팅방 생성
+
+router.get('/room/:id', enterRoom); //채팅방
+
+router.delete('/room/:id', removeRoom); //채팅방 제거
+
+router.post('/room/:id/chat', sendChat); //채팅 전송
+
+try {
+    fs.readdirSync('uploads');
+} catch (err) {
+    console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+    fs.mkdirSync('uploads');
+}
+const upload = multer({
+    storage: multer.diskStorage({
+        destination(req, file, done) {
+            done(null, 'uploads/');
+        },
+        filename(req, file, done) {
+            const ext = path.extname(file.originalname);
+            done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+        },
+    }),
+    limits: { fileSize: 5 * 1024 * 1024 },
+});
+router.post('/room/:id/gif', upload.single('gif'), sendGif);
 
 export default router;
