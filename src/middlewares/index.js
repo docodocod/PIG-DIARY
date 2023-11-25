320f61ee4ff76374bdd13b29cf8fc53f9d84e1b1
docodocod <dongan0618@naver.com>
2023-11-26 04:46:13 +0900

    commonJS_change

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,4 +1,4 @@
-export function isLoggedIn(req, res, next) {
+exports.isLoggedIn=(req, res, next)=>{
     if (req.isAuthenticated()) { //로그인이 되어있다면
         next(); //돌아가서 다음 함수 실행
     } else {
@@ -6,7 +6,7 @@ export function isLoggedIn(req, res, next) {
     }
 };
 
-export function isNotLoggedIn (req, res, next) {
+exports.isNotLoggedIn=(req, res, next)=>{
     if (!req.isAuthenticated()) { //로그인이 되어있지 않다면
         next(); //돌아가서 다음 함수 실행
     } else {
