6bdeea022703740d1073a7952f586206e1ee6af3
docodocod <dongan0618@naver.com>
2023-12-15 22:50:22 +0900

    passport modify

--- app.js
+++ app.js
@@ -5,20 +5,21 @@ const session=require("express-session");
 const morgan=require("morgan");
 const cookieParser=require("cookie-parser");
 const passport=require("passport");
-const ColorHash=require("color-hash");
+const ColorHash=require("color-hash").default;
 dotenv.config();
 
 const indexRouter=require("./src/routes/index.js");
 const authRouter=require("./src/routes/auth.js");
-const tokenTestRouter=require("./src/modules/verifyToken.js");
 const roomRouter=require("./src/routes/room.js");
-const {passportConfig}=require("./src/passport/index.js");
-const path=require("path");
 const {webSocket}=require("./src/utils/socket.js");
 const {sequelize}=require("./src/models/index.js");
+const passportConfig=require("./src/passport/index.js");
+const path=require("path");
+const tokenTestRouter=require("./src/modules/verifyToken.js");
 
 const app = express();
 passportConfig();
+
 app.set('port', process.env.SERVER_PORT || 8001); //Config.PORT를 앞에 붙여준 이유는 배포와 개발할때 서로 다른 포트를 사용할거라서
 app.set('view engine', 'html');
 nunjucks.configure('views', { //nunjucks 설정방법
@@ -27,26 +28,27 @@ nunjucks.configure('views', { //nunjucks 설정방법
 });
 
 sequelize.sync({ force: false }) //true로 하면 강제적으로 데이터베이스를 초기화하고 다시만든다.
-    .then(() => {
+    .then(() => { //sequelize를 실행 시켜 mySQL과 연결해준다.
         console.log('데이터베이스 연결 성공');
     })
     .catch((err) => {
         console.error(err);
     });
 
-app.use(morgan('dev'));
+const sessionMiddleware = session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+});
+app.use(morgan('dev')); // 데이터의 흐름을 자세히 보여줌
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
-    resave:false,
-    saveUninitialized:false,
-    secret:process.env.COOKIE_SECRET,
-    cookie:{
-        httponly:true,
-        secure:false,
-    },
-}));
+app.use(sessionMiddleware);
 app.use(passport.initialize());
 app.use(passport.session()); //passport->index.js로 넘어가서 deserialize로 넘어간다.
 
@@ -58,12 +60,7 @@ app.use('/token',tokenTestRouter);
 app.use('/auth',authRouter);
 app.use('/room',roomRouter);
 
-app.use((req, res, next) => {
-    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
-    next(error);
-});
-
-app.use((req, res, next) => {
+app.use((req, res, next) => {// 에러처리
     if (!req.session.color) {
         const colorHash = new ColorHash();
         req.session.color = colorHash.hex(req.sessionID);
@@ -71,6 +68,12 @@ app.use((req, res, next) => {
     }
     next();
 });
+app.use((req, res, next) => { //404 에러
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    next(error);
+});
+
+
 
 app.use((err, req, res, next) => {
     // set locals, only providing error in development
@@ -81,9 +84,9 @@ app.use((err, req, res, next) => {
     res.render('error');
 });
 
-const server=app.listen(process.env.SERVER_PORT,()=>{
+const server=app.listen(process.env.SERVER_PORT,()=>{ //웹서버 연결 확인
     console.log('Server Listening on 127.0.0.1:' + process.env.SERVER_PORT);
 });
 
-webSocket(server,app);
+webSocket(server,app,sessionMiddleware);
 
