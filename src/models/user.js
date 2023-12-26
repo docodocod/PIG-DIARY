75bfa8b7bf936e3e48a7329c59a9aa9a59266d3e
docodocod <dongan0618@naver.com>
2023-12-26 18:02:46 +0900

    unfollow_method_completed

--- src/models/user.js
+++ src/models/user.js
@@ -47,7 +47,7 @@ class User extends Sequelize.Model {
             as: 'Followings',
             through: 'Follow',
         });
-        db.User.belongsToMany(db.Post,{through: "like",as:"Liker"});
+        db.User.belongsToMany(db.Post,{through: "like"});
     };
 };
 
