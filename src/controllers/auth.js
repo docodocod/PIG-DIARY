670845922db2d22223ecabe420a00f12cdb5222a
donga <dongan0618@naver.com>
2024-01-16 18:28:12 +0900

    passwordFind_method_adding

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -3,43 +3,33 @@ const crypto=require('crypto');
 const dotenv=require('dotenv');
 const passport=require('passport');
 const jwt=require("jsonwebtoken");
+const nodeMailer=require("nodemailer");
+const {sendEmail}=require('../utils/emailSender');
+const {pbkdf2}=require("../utils/encrypt");
 dotenv.config();
 
-exports.loginTest=async(req,res,next)=>{
-    res.render("login",{title:"loginTest"});
-}
-
+//회원가입
 exports.join=async(req,res,next)=>{
     const {email,nick,password}=req.body;
-    try{
-        const exUser=await User.findOne({where:{email}});
-        if(exUser){
-            return res.redirect("/join?error=아이디가 존재 합니다"); //아이디가 존재할 시 return
+    try {
+        const exUser = await User.findOne({where: {email}});
+        if (exUser) {
+            return res.redirect("/join?error=아이디가 존재 합니다");
         }
-        const salt = process.env.SALT;
-        const iterations = parseInt(process.env.ITERATION);
-        const keyLength = 64; // 출력 길이
-
-
-        //비밀번호 암호화
-        crypto.pbkdf2(password, salt, iterations, keyLength, 'sha512', (err, derivedKey) => {
-            if (err) throw err;
-            const hashedPassword = derivedKey.toString('hex');
-            console.log('Hashed Password:', hashedPassword);
-            User.create({
-                email,
-                nick,
-                password: hashedPassword,
-                salt
-            });
-            return res.redirect('/');
+        const hashedPassword = pbkdf2(password);
+        await User.create({
+            email,
+            nick,
+            password: hashedPassword,
         });
+        res.redirect("/");
     }catch(error){
         console.error(error);
         return next(error);
     }
 };
 
+//로그인
 exports.login = (req, res, next) => {
     passport.authenticate('local', (authError, user, info) => {
         if (authError) {
@@ -56,14 +46,48 @@ exports.login = (req, res, next) => {
             }
             return res.redirect('/main');
         });
-    })(req, res, next); // 미들웨어 내의 미들웨어에는 (req, res, next)를 붙인다.
+    })(req, res, next);
 };
 
+//회원탈퇴
 exports.unregister=async (req,res,next)=>{
     await User.destroy({where:{id:req.user.id}});
     res.redirect('/');
 }
 
+//비밀번호 찾기
+exports.passwordFind=async(req,res)=> {
+    const userEmail = req.body.email;
+
+    // 이메일이 데이터베이스에 있는지 확인
+    const user = await User.findOne({where: {email: userEmail}});
+
+    if (!user) {
+        return res.status(404).send('해당 이메일로 가입된 사용자를 찾을 수 없습니다.');
+    }
+    const newPassword = Math.random().toString(36).slice(-8);
+    const hashedPassword=pbkdf2(newPassword);
+    try {
+        await User.update({password: hashedPassword}, {where: {email: userEmail}});
+        console.log("비밀번호 업데이트 성공");
+    }catch(err){
+        console.log(err);
+    }
+    // 비밀번호를 업데이트하고 이메일로 전송
+    sendEmail(userEmail, newPassword);
+    res.send('새로운 비밀번호가 이메일로 전송되었습니다.');
+}
+
+//비밀번호 초기화
+exports.passwordInit=async(req,res)=>{
+    const userId=req.body.id;
+    const password=req.body.password;
+    const newPassword=pbkdf2(password);
+    await User.update({password:newPassword},{where:{id:userId}});
+    res.redirect("/");
+}
+
+//로그아웃
 exports.logout = (req, res) => {
     req.logout(() => {
         res.redirect('/');
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,42 +1,60 @@
 const express=require("express");
 const passport=require("passport");
 const {isLoggedIn,isNotLoggedIn}=require("../middlewares/index.js");
-const {join,login,logout,loginTest,unregister}=require("../controllers/auth.js");
+const {join,login,logout,loginTest,unregister,passwordFind,passwordInit}=require("../controllers/auth.js");
 const router=express.Router();
 
-router.post('/join',isNotLoggedIn,join); //회원가입
 
-router.get("/loginTest",loginTest);
+/*회원가입 관련*/
 
-router.post("/login",isNotLoggedIn,login); //로그인
+//로그인
+router.post("/login",isNotLoggedIn,login);
 
-router.get('/logout',isLoggedIn,logout); //로그아웃
+//회원가입
+router.post('/join',isNotLoggedIn,join);
 
-router.get("/unregister",isLoggedIn,unregister); //회원탈퇴
+//카카오 로그인
+router.get("/kakao",passport.authenticate("kakao"));
 
-router.get("/kakao",passport.authenticate("kakao")); //카카오 로그인
+//구글 로그인
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+//네이버 로그인
+router.get('/naver', passport.authenticate('naver', { authType: 'reprompt' }));
+
+//로그아웃
+router.get('/logout',isLoggedIn,logout);
+
+
+/* 회원 */
+
+//회원탈퇴
+router.get("/unregister",isLoggedIn,unregister);
+
+//비밀번호 찾기
+router.post("/findPassword",passwordFind);
+
+//비밀번호 재설정
+router.post("/passwordInit",passwordInit);
+
 
 router.get('/kakao/callback', passport.authenticate('kakao', {
     failureRedirect: '/?error=카카오로그인 실패',
 }), (req, res) => {
-    res.redirect('/main'); // 성공 시에는 /로 이동
+    res.redirect('/main');
 });
 
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] })); //구글 로그인
 router.get(
     '/google/callback', passport.authenticate('google', { failureRedirect: '/?error=구글 로그인 실패' }),
     (req, res) => {
-        res.redirect('/');
+        res.redirect('/main');
     },
 );
-
-//* 네이버로 로그인하기 라우터 ***********************
-router.get('/naver', passport.authenticate('naver', { authType: 'reprompt' })); //네이버 로그인
 
 router.get(
     '/naver/callback', passport.authenticate('naver', { failureRedirect: '/?error=네이버 로그인 실패' }),
     (req, res) => {
-        res.redirect('/');
+        res.redirect('/main');
     },
 );
 
