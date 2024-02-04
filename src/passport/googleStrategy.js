3feed7938dc7b6cab21b6f05a27860c2ad37d54d
donga <dongan0618@naver.com>
2024-02-04 22:21:45 +0900

    social_login_done

--- src/passport/googleStrategy.js
+++ src/passport/googleStrategy.js
@@ -7,29 +7,26 @@ module.exports = () => {
     passport.use(
         new GoogleStrategy(
             {
-                clientID: process.env.GOOGLE_ID, // 구글 로그인에서 발급받은 REST API 키
+                clientID: process.env.GOOGLE_ID,
                 clientSecret: process.env.GOOGLE_SECRET,
-                callbackURL: process.env.GOOGLE_URL, // 구글 로그인 Redirect URI 경로
+                callbackURL: process.env.GOOGLE_URL,
             },
             async (accessToken, refreshToken, profile, done) => {
                 console.log('google profile : ', profile);
                 try {
                     const exUser = await User.findOne({
-                        // 구글 플랫폼에서 로그인 했고 & snsId필드에 구글 아이디가 일치할경우
                         where: { snsId: profile.id, provider: 'google' },
                     });
-                    // 이미 가입된 구글 프로필이면 성공
                     if (exUser) {
-                        done(null, exUser); // 로그인 인증 완료
+                        done(null, exUser);
                     } else {
-                        // 가입되지 않는 유저면 회원가입 시키고 로그인을 시킨다
                         const newUser = await User.create({
-                            email: profile?.email[0].value,
+                            email: profile._json.email,
                             nick: profile.displayName,
                             snsId: profile.id,
                             provider: 'google',
                         });
-                        done(null, newUser); // 회원가입하고 로그인 인증 완료
+                        done(null, newUser);
                     }
                 } catch (error) {
                     console.error(error);
--- src/passport/kakaoStrategy.js
+++ src/passport/kakaoStrategy.js
@@ -15,16 +15,15 @@ module.exports = () => {
                         where: {snsId: profile.id, provider: 'kakao'},
                     });
                     if (exUser) {
-                        done(null, exUser); // 로그인 인증 완료
+                        done(null, exUser);
                     } else {
-                        // 가입되지 않는 유저면 회원가입 시키고 로그인을 시킨다
                         const newUser = await User.create({
                             email: profile._json && profile._json.kakao_account_email,
                             nick: profile.displayName,
                             snsId: profile.id,
                             provider: 'kakao',
                         });
-                        done(null, newUser); // 회원가입하고 로그인 인증 완료
+                        done(null, newUser);
                     }
                 } catch (error) {
                     console.error(error);
