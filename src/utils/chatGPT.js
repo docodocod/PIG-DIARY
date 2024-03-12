d1200ca06d0ade597ceb347d933ee09f59da0705
donga <dongan0618@naver.com>
2024-03-12 15:00:58 +0900

    chat-gpt-update

--- src/utils/chatGPT.js
+++ src/utils/chatGPT.js
@@ -1,17 +1,38 @@
 require('dotenv').config();
+const OpenAI = require("openai");
+const openai = new OpenAI({
+    apiKey: process.env.GPT_OPEN_API_KEY
+});
+const chatList=[];
 
-//chat-gpt function
-async function callChatGPT(prompt) {
-    const OpenAI = require("openai");
+async function callChatGPT(question) {
+    chatList.push({
+        role:"user",
+        content:question
+    })
 
-    const openai = new OpenAI({
-        apiKey: process.env.GPT_OPEN_API_KEY
-    });
     const chatCompletion = await openai.chat.completions.create({
         model: "gpt-3.5-turbo",
-        messages: [{role: "user", content: prompt}],
+        messages: chatList,
         max_tokens:500,
     });
+
+    const summary=await summarized(chatCompletion.choices[0].message.content);
+    chatList.push({
+        role:"assistant",
+        content: summary,
+    })
+    console.log(chatList);
     return chatCompletion;
 }
+
+//answer 요약 메서드
+async function summarized(answer) {
+    const answerCompletion = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: [{role: 'assistant', content: `${answer} 이 문장을 정말 최대한 짧게 요약해줘`}],
+    });
+    return answerCompletion.choices[0].message.content;
+}
+
 module.exports = { callChatGPT };
